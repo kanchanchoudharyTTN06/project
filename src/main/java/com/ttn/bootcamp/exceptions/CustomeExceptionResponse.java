18a2ba13b6848a4877e972150b61ffa54926d6de
kanchanchoudharyTTN06 <kanchan.choudhary@tothenew.com>
2021-10-28 18:05:52 +0530

    removed unused code

--- src/main/java/com/ttn/bootcamp/exceptions/CustomeExceptionResponse.java
+++ src/main/java/com/ttn/bootcamp/exceptions/CustomeExceptionResponse.java
@@ -1,13 +1,9 @@
 package com.ttn.bootcamp.exceptions;
 
-import java.util.Map;
-import java.util.Objects;
-
 public class CustomeExceptionResponse {
     private Object message;
     private int statusCode;
     private String status = "error";
-    private Map<String, String> messages;
 
     public Object getMessage() {
         return message;
@@ -24,15 +20,6 @@ public class CustomeExceptionResponse {
 
     public CustomeExceptionResponse setStatusCode(int statusCode) {
         this.statusCode = statusCode;
-        return this;
-    }
-
-    public Map<String, String> getMessages() {
-        return messages;
-    }
-
-    public CustomeExceptionResponse setMessages(Map<String, String> messages) {
-        this.messages = messages;
         return this;
     }
 
