bcc81ee6e286531fcefb6acb5165361c593653bb
kanchanchoudharyTTN06 <kanchan.choudhary@tothenew.com>
2021-11-02 17:24:34 +0530

    Login Security

--- src/main/java/com/ttn/bootcamp/security/UserRepository1.java
+++ src/main/java/com/ttn/bootcamp/security/UserRepository1.java
@@ -2,6 +2,6 @@ package com.ttn.bootcamp.security;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class UserRepository1 extends JpaRepository<UserLogin, Long>{
+public interface UserRepository1 extends JpaRepository<UserLogin, Long>{
     UserLogin findByUsername(String username);
 }
